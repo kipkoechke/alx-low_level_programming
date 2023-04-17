8213d8c9d0110faf09dbdf85363cd20bea74d280
kipkoechke <chepkwonyke1@gmail.com>
2023-04-17 09:36:00 -0700

    Defined structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Data type of a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}

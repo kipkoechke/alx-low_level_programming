0bc9da970cbf019833882f08cb79eb3ad1fc8ace
kipkoechke <chepkwonyke1@gmail.com>
2023-05-02 06:43:13 -0700

    Reversed list

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+  * reverse_listint - function that reverses a linked list.
+  * @head: head to the list
+  * Return: reversed list
+  */
+listint_t *reverse_listint(listint_t **head)
+{
+	head--;
+	return (0);
+}

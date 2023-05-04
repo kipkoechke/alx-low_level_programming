e9482b8821e23c1680d01269ae28fa96d07832fb
kipkoechke <chepkwonyke1@gmail.com>
2023-05-04 07:06:22 -0700

    COnverted a binary number to a string

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * binary_to_uint - function that converts a binary number to an unsigned int
+ * @b: string to convert
+ * Return: the converted number, or 0 if
+ * -> there is one or more chars in the string b that is not 0 or 1
+ * -> b is NULL
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int value = 0;
+
+	if (b == NULL)
+		return (0);
+	while (*b != '\0')
+	{
+		value = value << 1;
+		if (*b != '1' && *b != '0')
+			return (0);
+		else if (*b == '1')
+			value = value | 1;
+		b++;
+	}
+	return (value);
+}
